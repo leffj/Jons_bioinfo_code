df9699d619a31aa2dfde0ca2bbec068796a007dc
Jon <jonathan.leff@colorado.edu>
2014-07-14 15:51:56 -0600

    randomize sequence order

---
+++ random_fasta_order.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+__author__ = "Jonathan Leff"
+__version__ = "0.0.1"
+__email__ = "jleff@symbiota.ag"
+
+"""Filter fasta/q file to keep sequences from a list of samples"""
+
+from Bio import SeqIO
+import random
+import argparse
+
+
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-i', '--input_fp', required=True,
+		help='The input filepath')
+	parser.add_argument('-o', '--output_fp', required=True,
+		help='The output filepath')
+	parser.add_argument('-s', '--samples_fp', required=True,
+		help='Samples to keep')
+	parser.add_argument('-t','--file_type',\
+		help='Filetype detected from file extension, but can be overridden here.'\
+		'The input sequences file type (either "fastq" or "fasta")')
+
+	args = parser.parse_args()
+
+	#randomize_seq_order(args.input_fp,args.output_fp)
+
+def get_file_size(FileName):
+	File = open(FileName,'U')
+	Pos = File.tell()
+	File.seek(0, 2)
+	FileSize = File.tell()
+	File.seek(Pos)
+	File.close()
+	return FileSize
+
+def display_progress(position,fileSize):
+	pct = (100.0*position)/fileSize
+	sys.stdout.write('\r%5.1f%%' % (pct))
+	sys.stdout.flush()
+
+ 
+def randomize_seq_order(input_fp,output_fp):
+	""" Take a fasta file and randomize the order of the the sequences. This is
+	used when doing clustering when there isn't a logical way to order the sequences
+	for UCLUST. This is an effort to remove bias for clusters being created around one
+	dataset or another.
+	"""
+
+	seqs = []
+	for seq_record in SeqIO.parse(open(input_fp,'rU'), "fasta"):
+		seqs.append(seq_record)
+	randIds = random.sample(range(0,len(seqs)),len(seqs))
+	seqsRand = []
+	for i in randIds:
+		seqsRand.append(seqs[i])
+	out = open(output_fp,'w')
+	SeqIO.write(seqsRand, out, "fasta")
+	out.close()
+
+
+if __name__ == "__main__":
+		main()
+
+
